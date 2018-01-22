fc72bcc7d886b9bc5114e11881ee0b0910f703ce
Jason Henderson <jasonhenderson@Jasons-MacBook-Pro.local>
2018-01-21 22:47:36 -0800

    converted from urllib2 to urllib.request

--- c9addmember/invite/adduser.py
+++ c9addmember/invite/adduser.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # Imports
-import urllib2, os, sys
+import urllib.request, os, sys
 from cookielib import CookieJar
 
 USER_AGENT = "/kinoshitajona/c9community/add_users/script/v0.0.4"
@@ -14,12 +14,12 @@ def add_user(ADD_USER_EMAIL):
         TEAM_NAME = os.environ['C9_TEAM_NAME']
         
         cj = CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-        urllib2.install_opener(opener)
+        opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+        urllib.request.install_opener(opener)
         
         login_data = r'{"username": "' + USER_NAME + '", "password": "' + PASSPHRASE + '"}'
         
-        req = urllib2.Request("https://c9.io/auth/login",
+        req = urllib.request.Request("https://c9.io/auth/login",
                               data=login_data)
         
         req.add_header("User-Agent",USER_AGENT)
@@ -27,17 +27,17 @@ def add_user(ADD_USER_EMAIL):
         req.add_header("Referer","https://c9.io/login")
         
         # Send the request here. We don't need anything from the response, just the cookies which are handled for us.
-        result1 = urllib2.urlopen(req)
+        result1 = urllib.request.urlopen(req)
         
         # Second request, this time we need a token this one is GET request as data=None
-        req = urllib2.Request("https://c9.io/api/nc/auth?client_id=profile_direct&responseType=direct&login_hint=&immediate=1")
+        req = urllib.request.Request("https://c9.io/api/nc/auth?client_id=profile_direct&responseType=direct&login_hint=&immediate=1")
         
         # Set headers again. Different referrer
         req.add_header("User-Agent",USER_AGENT)
         req.add_header("Content-Type","application/json")
         
         # Send GET request
-        result2 = urllib2.urlopen(req)
+        result2 = urllib.request.urlopen(req)
         
         # Read the token from the response.
         token = result2.read() # Token
@@ -47,14 +47,14 @@ def add_user(ADD_USER_EMAIL):
         email_data = r'{"email": "' + ADD_USER_EMAIL + '"}'
         
         # this is a POST request to invite the email address to your team.
-        req = urllib2.Request("https://api.c9.io/user/org/" + TEAM_NAME + "/invite?access_token=" + token,
+        req = urllib.request.Request("https://api.c9.io/user/org/" + TEAM_NAME + "/invite?access_token=" + token,
                               data=email_data)
         
         req.add_header("User-Agent",USER_AGENT)
         req.add_header("Content-Type","application/json")
         
         # Send the POST request
-        result3 = urllib2.urlopen(req)
+        result3 = urllib.request.urlopen(req)
     except:
         return False
     
